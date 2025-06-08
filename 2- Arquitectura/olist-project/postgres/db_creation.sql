ac3eb03199115b7133e2cfb067a86a7b8a649b31
camila <alv_camila@outlook.com>
2025-06-07 22:07:09 -0300

    chore: sp scripts for etl jobs

--- 2- Arquitectura/olist-project/postgres/db_creation.sql
+++ 2- Arquitectura/olist-project/postgres/db_creation.sql
@@ -121,3 +121,159 @@ UNIQUE (product_category_name);
 ALTER TABLE public.geolocation
 ADD CONSTRAINT unique_geolocation_zip_code_prefix
 UNIQUE (geolocation_zip_code_prefix);
+
+-- SP ETL JOBS
+CREATE OR REPLACE PROCEDURE public.transfer_data_from_stg_to_customers()
+LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    INSERT INTO public.customers (customer_id, customer_zip_code_prefix, customer_city, customer_state)
+    SELECT
+        customer_id::uuid,
+        customer_zip_code_prefix::text,
+        customer_city,
+        customer_state
+    FROM public.stg_customers;
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE public.transfer_data_from_stg_to_orders() 
+LANGUAGE PLPGSQL 
+AS 
+$$
+BEGIN
+    INSERT INTO public.orders (order_id, order_status, order_purchase_timestamp, order_approved_at,
+                               order_delivered_carrier_date, order_delivered_customer_date, order_estimated_delivery_date, customer_id)
+    SELECT
+		order_id::uuid,
+		order_status,
+        order_purchase_timestamp::timestamp,
+        order_approved_at::timestamp,
+        order_delivered_carrier_date::timestamp,
+        order_delivered_customer_date::timestamp,
+        order_estimated_delivery_date::timestamp,
+        customer_id::uuid
+    FROM public.stg_orders;
+
+END;
+$$;
+
+
+CREATE OR REPLACE PROCEDURE public.transfer_data_from_stg_to_products() 
+LANGUAGE PLPGSQL 
+AS 
+$$
+BEGIN
+    INSERT INTO public.products 
+	(product_id, product_category_name, product_name_length, product_description_length, product_photos_qty, product_weight_g, product_length_cm, product_height_cm, product_width_cm)
+    SELECT
+		product_id::uuid,
+		product_category_name,
+		product_name_length::double precision,
+		product_description_length::double precision,
+		product_photos_qty::double precision,
+		product_weight_g::double precision,
+		product_length_cm::double precision,
+		product_height_cm::double precision,
+		product_width_cm::double precision
+    FROM public.stg_products;
+
+END;
+$$;
+
+
+CREATE OR REPLACE PROCEDURE public.transfer_data_from_stg_to_geolocation() 
+LANGUAGE PLPGSQL 
+AS 
+$$
+BEGIN
+    INSERT INTO public.geolocation(
+	geolocation_id, geolocation_zip_code_prefix, geolocation_lat, geolocation_lng, geolocation_city, geolocation_state)
+	SELECT
+		uuid_generate_v4() geolocation_id,
+		geolocation_zip_code_prefix::character varying,
+		geolocation_lat::double precision,
+		geolocation_lng::double precision,
+		geolocation_city,
+		geolocation_state
+    FROM public.stg_geolocation;
+
+END;
+$$;
+
+
+CREATE OR REPLACE PROCEDURE public.transfer_data_from_stg_to_order_items() 
+LANGUAGE PLPGSQL 
+AS 
+$$
+BEGIN
+    INSERT INTO public.order_items(
+	item_id, order_item_id, shipping_limit_date, price, freight_value, order_id, product_id, seller_id)
+	SELECT
+		uuid_generate_v4() item_id,
+		order_item_id::integer,
+		shipping_limit_date::timestamp without time zone,
+		price::numeric(10,2),
+		freight_value::numeric(10,2),
+		order_id::uuid,
+		product_id::uuid,
+		seller_id::uuid
+    FROM public.stg_order_items;
+
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE public.transfer_data_from_stg_to_order_payments() 
+LANGUAGE PLPGSQL 
+AS 
+$$
+BEGIN
+    INSERT INTO public.order_payments(
+	payment_id, payment_sequential, payment_type, payment_installments, payment_value, order_id)
+	SELECT
+		uuid_generate_v4() payment_id,
+		payment_sequential::integer,
+		payment_type::character varying(255),
+		payment_installments::integer,
+		payment_value::numeric(10,2),
+		order_id::uuid
+    FROM public.stg_order_payments;
+
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE public.transfer_data_from_stg_to_order_reviews() 
+LANGUAGE PLPGSQL 
+AS 
+$$
+BEGIN
+    INSERT INTO public.order_reviews(
+	review_unique_id, review_id, review_score, review_creation_date, review_answer_timestamp, order_id)
+	SELECT
+		uuid_generate_v4() review_unique_id,
+		review_id::uuid,
+		review_score::integer,
+		review_creation_date::timestamp without time zone,
+		review_answer_timestamp::timestamp without time zone,
+		order_id::uuid
+    FROM public.stg_order_reviews;
+
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE public.transfer_data_from_stg_to_product_category_name_tr() 
+LANGUAGE PLPGSQL 
+AS 
+$$
+BEGIN
+    INSERT INTO public.product_category_name_translation(
+	translation_id, product_category_name, product_category_name_english)
+	SELECT
+		uuid_generate_v4() translation_id,
+		product_category_name::character varying(255),
+		product_category_name_english::character varying(255)
+    FROM public.stg_product_category_name_translation;
+
+END;
+$$;
